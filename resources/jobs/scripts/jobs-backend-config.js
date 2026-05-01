3a411c533b9023d8ff5a04e367d1bc46e5270ef6
Cassandre Arkema <cassandre.arkema@gmail.com>
2026-05-01 16:42:35 -0500

    Update jobs-backend-config.js

--- resources/jobs/scripts/jobs-backend-config.js
+++ resources/jobs/scripts/jobs-backend-config.js
@@ -1,6 +1,6 @@
 window.JOBS_BACKEND_CONFIG = {
   // backendUrl is the Apps Script Web App URL used by public browser forms and private local admin tools.
-  backendUrl: "https://script.google.com/macros/s/AKfycbx09Ux0WVavkP0200gQ3ibWEowCnlNpV6tEGlrg20GqwJXlPoyiCyjKkiggHaXAzyg/exec",
+  backendUrl: "https://script.google.com/macros/s/AKfycbxH5LocyhPQK99aUoai4nwCILe9zmIFnz2BTxrLRu07Ag_tSL3LQBY6HXegbM877MWz/exec",
   // adminToken is only for private/local admin use.
   // Do not commit a real admin token to a public repo.
   adminToken: ""

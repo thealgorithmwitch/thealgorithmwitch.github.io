568bac5d74b17e6488063742c471a92eb849ccf2
Cassandre Arkema <cassandre.arkema@gmail.com>
2026-05-06 13:35:16 -0500

    Update generate-blog.mjs

--- generate-blog.mjs
+++ generate-blog.mjs
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 
 const FEED_URL =
   process.env.SUBSTACK_FEED_URL ||
-  "https://feedless.org/feed-builder?url=https:%2F%2Fthealgorithmwitch.substack.com%2Ffeed";
+  "https://feeds.feedburner.com/substack/Y6jFx8FF2q";
 const SITE_ORIGIN = 'https://thealgorithmwitch.com';
 const BLOG_URL = `${SITE_ORIGIN}/blog/`;
 const SUBSTACK_URL = 'https://thealgorithmwitch.substack.com/';
